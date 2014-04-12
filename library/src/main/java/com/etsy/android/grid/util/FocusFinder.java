03cc8f6bd5a2e6825d107c5766aec65e0fdb1000
calmer <calmer91@gmail.com>
2014-04-12 22:48:46 +0800

    remove unused import

--- library/src/main/java/com/etsy/android/grid/util/FocusFinder.java
+++ library/src/main/java/com/etsy/android/grid/util/FocusFinder.java
@@ -1,33 +1,12 @@
 
 package com.etsy.android.grid.util;
 
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import android.graphics.Rect;
 import android.util.SparseArray;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
